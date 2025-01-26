45080ceb7fb8446650f65befae343cb8a50ece2f
Bill Yang <45103519+goldflag@users.noreply.github.com>
2025-01-25 23:25:28 -0800

    add topbar

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  env: {
+    BACKEND_URL: process.env.BACKEND_URL,
+  },
 };
 
 module.exports = nextConfig;
--- client/src/app/components/Header/Header.tsx
+++ client/src/app/components/Header/Header.tsx
@@ -2,6 +2,26 @@
 import { Circle } from "@phosphor-icons/react";
 import { useQuery } from "@tanstack/react-query";
 
+import { useStore } from "@/lib/store";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { CustomDateRangePicker } from "./CustomDateRangePicker";
+
+const DATE_OPTIONS = [
+  { value: "1d", label: "Today" },
+  { value: "7d", label: "Last 7 days" },
+  { value: "30d", label: "Last 30 days" },
+  { value: "90d", label: "Last 3 months" },
+];
+
 export function Header() {
   const { data } = useQuery<{ count: number }>({
     queryKey: ["active-sessions"],
@@ -10,15 +30,47 @@ export function Header() {
         res.json()
       ),
   });
+  const { date, setDate } = useStore();
 
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex items-center gap-2 text-xl font-bold">
-        <div>Tomato.gg</div>
+    <div className="flex items-center justify-between py-2">
+      <div className="flex gap-3">
+        <div className="flex items-center gap-2 text-xl font-bold">
+          <div>Tomato.gg</div>
+        </div>
+        <div className="flex items-center gap-1 text-base text-neutral-600 dark:text-neutral-400">
+          <Circle size={12} weight="fill" color="hsl(var(--green-500))" />
+          {data?.count} users online
+        </div>
       </div>
-      <div className="text-base text-neutral-600 dark:text-neutral-400">
-        <Circle size={24} weight="fill" color="#FF6600" />
-        {data?.count}
+      <div className="flex items-center gap-2">
+        <div className="flex items-center">
+          <Button variant="outline" size="icon">
+            <ChevronLeft />
+          </Button>
+          <Button variant="outline" size="icon">
+            <ChevronRight />
+          </Button>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Button variant="outline">
+              {DATE_OPTIONS.find((option) => option.value === date)?.label}
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {DATE_OPTIONS.map((option) => (
+              <DropdownMenuItem
+                onClick={() => setDate(option.value)}
+                key={option.value}
+              >
+                {option.label}
+              </DropdownMenuItem>
+            ))}
+            <DropdownMenuSeparator />
+            <CustomDateRangePicker />
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </div>
   );
