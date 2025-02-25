bf43e2f7bbf5f31b8d4e65ebaa8ce290ca3e8fa0
Bill Yang <45103519+goldflag@users.noreply.github.com>
2025-02-24 20:02:06 -0800

    improve filter styling

--- client/src/app/[site]/components/Filters/Filters.tsx
+++ client/src/app/[site]/components/Filters/Filters.tsx
@@ -1,18 +1,73 @@
-import { useStore } from "../../../../lib/store";
+import { X } from "lucide-react";
+import {
+  Filter,
+  FilterParameter,
+  removeFilter,
+  useStore,
+} from "../../../../lib/store";
+
+function getParameterNameLabel(parameter: FilterParameter) {
+  switch (parameter) {
+    case "country":
+      return "Country";
+    case "device_type":
+      return "Device Type";
+    case "operating_system":
+      return "Operating System";
+    case "browser":
+      return "Browser";
+    case "referrer":
+      return "Referrer";
+    case "pathname":
+      return "Pathname";
+    default:
+      return parameter;
+  }
+}
+
+function getParameterValueLabel(filter: Filter) {
+  switch (filter.parameter) {
+    case "country":
+      return filter.value;
+    case "device_type":
+      return filter.value;
+    case "operating_system":
+      return filter.value;
+    case "browser":
+      return filter.value;
+    case "referrer":
+      return filter.value;
+    case "pathname":
+      return filter.value;
+    default:
+      return filter.value;
+  }
+}
 
 export function Filters() {
   const { filters } = useStore();
   if (filters.length === 0) return null;
 
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 mb-3">
       {filters.map((filter) => (
         <div
           key={filter.parameter}
-          className="p-1 rounded-md bg-neutral-850 text-neutral-400 flex items-center gap-1"
+          className="px-2 py-1 rounded-md bg-neutral-850 text-neutral-400 flex items-center gap-1 text-sm"
         >
-          <div className="text-xs">{filter.parameter}</div>
-          <div className="text-xs">{filter.value}</div>
+          <div className="text-neutral-300">
+            {getParameterNameLabel(filter.parameter)}
+          </div>
+          <div className="text-emerald-400 font-medium">is</div>
+          <div className="text-neutral-100 font-medium">
+            {getParameterValueLabel(filter)}
+          </div>
+          <div
+            className="text-neutral-400 cursor-pointer hover:text-neutral-200"
+            onClick={() => removeFilter(filter)}
+          >
+            <X size={16} strokeWidth={3} />
+          </div>
         </div>
       ))}
     </div>
