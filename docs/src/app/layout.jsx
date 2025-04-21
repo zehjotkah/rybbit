6738dc8f4a609d0d4075f7cd0d69bae05af22e11
Bill Yang <45103519+goldflag@users.noreply.github.com>
2025-04-20 18:07:14 -0700

    Update favicon and apple icon images, and modify package.json and package-lock.json to include Tailwind CSS and PostCSS dependencies

--- docs/src/app/layout.jsx
+++ docs/src/app/layout.jsx
@@ -2,25 +2,35 @@
 import { Footer, Layout, Navbar } from 'nextra-theme-docs'
 import { Banner, Head } from 'nextra/components'
 import { getPageMap } from 'nextra/page-map'
+import Image from 'next/image'
 import 'nextra-theme-docs/style.css'
+import './globals.css'
+import {
+  Tilt_Warp,
+} from "next/font/google";
+
+const tilt_wrap = Tilt_Warp({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export const metadata = {
-  metadataBase: new URL('https://nextra.site'),
+  metadataBase: new URL('https://rybbit.io'),
   title: {
-    template: '%s - Nextra'
+    template: '%s - Rybbit'
   },
-  description: 'Nextra: the Next.js site builder',
-  applicationName: 'Nextra',
+  description: 'Rybbit - open source web analytics',
+  applicationName: 'Rybbit',
   generator: 'Next.js',
   appleWebApp: {
-    title: 'Nextra'
+    title: 'Rybbit'
   },
   other: {
     'msapplication-TileImage': '/ms-icon-144x144.png',
     'msapplication-TileColor': '#fff'
   },
   twitter: {
-    site: 'https://nextra.site'
+    site: 'https://rybbit.io'
   }
 }
 
@@ -28,13 +38,13 @@ export default async function RootLayout({ children }) {
   const navbar = (
     <Navbar
       logo={
-        <div>
-          <b>Nextra</b>{' '}
-          <span style={{ opacity: '60%' }}>The Next Docs Builder</span>
+        <div className={`text-xl flex items-center gap-1.5 ${tilt_wrap.className}`}>
+          <Image src="/rybbit.png" alt="Rybbit" width={22} height={22} />
+          rybbit.
         </div>
       }
-      chatLink="https://discord.gg/hEM84NMkRv"
-      projectLink="https://github.com/goldflag/frogstats"
+      chatLink="https://discord.gg/DEhGb4hYBj"
+      projectLink="https://github.com/goldflag/rybbit"
     />
   )
   const pageMap = await getPageMap()
@@ -43,9 +53,9 @@ export default async function RootLayout({ children }) {
       <Head faviconGlyph="✦" />
       <body>
         <Layout
-          banner={<Banner storageKey="Frogstats">Frogstats Alpha</Banner>}
+          banner={<Banner storageKey="Rybbit">Rybbit Alpha</Banner>}
           navbar={navbar}
-          footer={<Footer>Copyright {new Date().getFullYear()} © Frogstats.</Footer>}
+          footer={<Footer>Copyright {new Date().getFullYear()} © Rybbit.</Footer>}
           editLink="Edit this page on GitHub"
           docsRepositoryBase="https://github.com/shuding/nextra/blob/main/examples/docs"
           sidebar={{ defaultMenuCollapseLevel: 1 }}
