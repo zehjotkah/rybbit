b0ad344162e6513b9c834ad47ca3155b93598406
Bill Yang <45103519+goldflag@users.noreply.github.com>
2025-04-22 20:10:54 -0700

    Update docker-entrypoint.sh to force database migrations during startup

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-# Run migrations explicitly using the npm script
-echo "Running database migrations...."
-npm run db:push
+# Run migrations explicitly using the npm script, forcing changes
+echo "Running database migrations..."
+npm run db:push -- --force
 
 # Start the application
 echo "Starting application..."
