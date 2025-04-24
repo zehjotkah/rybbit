a48297e3285d1cc0fd78053620c79a96e0abb09d
Bill Yang <45103519+goldflag@users.noreply.github.com>
2025-04-23 23:11:21 -0700

    Refactor Header and UsageBanners components for improved readability and structure
    - Simplified the return statement in the `Header` component for better clarity.
    - Enhanced the layout of the `UsageBanners` component by wrapping alerts in a div for consistent spacing.
    - Updated alert descriptions to provide clearer messaging regarding event limits based on user ownership status.

--- client/src/app/[site]/components/Header/Header.tsx
+++ client/src/app/[site]/components/Header/Header.tsx
@@ -6,13 +6,5 @@ import { UsageBanners } from "./UsageBanners";
 export function Header() {
   const { user } = userStore();
 
-  return (
-    <div className="flex flex-col">
-      {user && (
-        <div className="m-4 mb-0">
-          <UsageBanners />
-        </div>
-      )}
-    </div>
-  );
+  return <div className="flex flex-col">{user && <UsageBanners />}</div>;
 }
