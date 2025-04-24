c981efd7a28397648722f1e8bd9c8a38eb0ceca7
Bill Yang <45103519+goldflag@users.noreply.github.com>
2025-04-23 19:42:35 -0700

    Update MapPage layout for improved responsiveness and z-index management
    - Adjusted the height of the main container in `MapPage` to ensure full viewport usage.
    - Enhanced z-index values for elements to improve layering and visibility of the map and controls.
    - Wrapped the `MapComponent` in a new div to ensure proper positioning and interaction.

--- client/src/app/[site]/map/page.tsx
+++ client/src/app/[site]/map/page.tsx
@@ -9,11 +9,11 @@ export default function MapPage() {
   const [mapMode, setMapMode] = useState<"total" | "perCapita">("total");
 
   return (
-    <div className="relative w-full">
-      <div className="p-4">
+    <div className="relative w-full h-[calc(100vh-45px)]">
+      <div className="p-4 relative z-50">
         <SubHeader />
       </div>
-      <div className="absolute bottom-4 left-4  z-10">
+      <div className="absolute bottom-4 left-4 z-50">
         <div className="flex items-center gap-3 bg-neutral-900 rounded-md px-3 py-2 text-sm">
           <span
             className={mapMode === "total" ? "text-white" : "text-neutral-400"}
@@ -36,7 +36,9 @@ export default function MapPage() {
           </span>
         </div>
       </div>
-      <MapComponent height="calc(100vh - 45px - 76px - 40px)" mode={mapMode} />
+      <div className="absolute top-0 left-0 right-0 bottom-0 z-10">
+        <MapComponent height="calc(100vh - 45px - 40px)" mode={mapMode} />
+      </div>
     </div>
   );
 }
