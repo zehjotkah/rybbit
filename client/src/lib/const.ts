4f5eb6f36d5a7881022fb8f4d5376a72a1ed0ff1
Bill Yang <45103519+goldflag@users.noreply.github.com>
2025-05-07 10:57:37 -0700

    Origin fallback (#136)
    * origin fallback
    
    * Revert "origin fallback"
    
    This reverts commit ac5e17aff4d5f07d74518c199bbce6d82123ec74.
    
    * origin fallback

--- client/src/lib/const.ts
+++ client/src/lib/const.ts
@@ -1,6 +1,8 @@
 export const BACKEND_URL =
   process.env.NEXT_PUBLIC_BACKEND_URL === "http://localhost:3001"
     ? "http://localhost:3001"
-    : `${process.env.NEXT_PUBLIC_BACKEND_URL}/api`;
+    : `${
+        process.env.NEXT_PUBLIC_BACKEND_URL || globalThis.location.origin
+      }/api`;
 export const IS_CLOUD = process.env.NEXT_PUBLIC_CLOUD === "true";
 export const IS_DEMO = process.env.NEXT_PUBLIC_DEMO === "true";
