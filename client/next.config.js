fdaa16c95809344476560a00af7d30606f71d7d5
Andrii Zontov <loginmanx@gmail.com>
2025-05-12 23:02:11 +0200

    feat(client): Use version from `package.json` file (#212)

--- client/next.config.js
+++ client/next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
     NEXT_PUBLIC_DISABLE_SIGNUP: process.env.NEXT_PUBLIC_DISABLE_SIGNUP,
+    NEXT_PUBLIC_APP_VERSION: process.env.npm_package_version
   },
 };
 
--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -1,17 +1,16 @@
 import Link from "next/link";
 
-const VERSION = "v0.1.4";
-
 export function Footer() {
-  const gitSha = process.env.NEXT_PUBLIC_GIT_SHA;
+  const APP_VERSION = process.env.NEXT_PUBLIC_APP_VERSION;
+
   return (
     <div className="flex justify-center items-center h-12 text-neutral-400 gap-4 text-xs">
       <p>© 2025 Rybbit</p>
       <Link
-        href={`https://github.com/rybbit-io/rybbit/releases/tag/${VERSION}`}
+        href={`https://github.com/rybbit-io/rybbit/releases/tag/v${APP_VERSION}`}
         className="hover:text-neutral-300"
       >
-        {VERSION}
+        v{APP_VERSION}
       </Link>
       <Link href="https://rybbit.io/docs" className="hover:text-neutral-300">
         Docs
