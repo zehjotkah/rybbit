da96da9af716c2aacdb87b0951c4754f3f1a36b5
Bill Yang <billyang@umich.edu>
2025-10-18 23:21:13 -0700

    Remove deprecated documentation and assets
    - Deleted various files related to documentation, including configuration files, images, and components that are no longer in use.
    - Cleaned up the project by removing unused assets and files to streamline the codebase and improve maintainability.

--- docs/src/content/blog/_meta.js
+++
@@ -1,6 +0,0 @@
-export default {
-  "5k-stars":
-    "How my open source SaaS got 5,000 Github stars in 9 days with zero marketing effort",
-  "website-traffic-drop":
-   "Why did my website traffic drop?"
-};
--- docs/src/content/self-hosting-guides/_meta.js
+++
@@ -1,4 +0,0 @@
-export default {
-  "custom-nginx": "Custom Nginx",
-  "nginx-proxy-manager": "Nginx Proxy Manager",
-};
