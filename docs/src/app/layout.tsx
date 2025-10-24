4eb5b604023aaa8e51a98a35f3612b396af8ec0a
Bill Yang <billyang@umich.edu>
2025-10-24 14:56:25 -0700

    remove gtm

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -77,52 +77,6 @@ const isDev = process.env.NODE_ENV === "development";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning className="dark">
-      <head>
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NB4M75CR');`,
-          }}
-        />
-        <Script
-          id="reddit-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `!function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);rdt('init','a2_hpzt3lz1c1f0');rdt('track', 'PageVisit');`,
-          }}
-        />
-        <Script
-          id="twitter-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
-},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
-a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
-twq('config','qj0po');`,
-          }}
-        />
-        <Script
-          id="facebook-pixel"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window,document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '816836311029789');
-fbq('track', 'PageView');`,
-          }}
-        />
-      </head>
       <Script
         src="https://demo.rybbit.io/api/script.js"
         data-site-id="21"
@@ -135,21 +89,7 @@ fbq('track', 'PageView');`,
           "data-api-key": process.env.NEXT_PUBLIC_RYBBIT_API_KEY,
         })}
       />
-      <script
-        defer
-        data-website-id="68e0a805cbd31e9b4a24cca1"
-        data-domain="rybbit.io"
-        src="https://datafa.st/js/script.js"
-      ></script>
       <body className={`flex flex-col min-h-screen ${inter.variable} font-sans`}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NB4M75CR"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
         <RootProvider
           theme={{
             forcedTheme: "dark",
